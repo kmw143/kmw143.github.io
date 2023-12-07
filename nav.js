22adabbf9e61738d8f0aac0548b5829947ca4fa5
Kami Wang <kmw143@duke.edu>
2023-12-08 01:05:35 +0800

    Draft 5 - 12/8 1:05AM

---
+++ nav.js
@@ -0,0 +1,12 @@
+// From https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("navLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
