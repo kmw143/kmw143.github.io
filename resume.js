ce7fce431f7222eb08fd733d427768657fe0823d
Kami Wang <kmw143@duke.edu>
2023-12-08 13:10:40 +0800

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,4 +1,4 @@
-// https://www.w3schools.com/howto/howto_js_accordion.asp
+// From https://www.w3schools.com/howto/howto_js_accordion.asp
 
 var acc = document.getElementsByClassName("accordion");
 var i;
