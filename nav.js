0ac7d3013dc5fbbd727b2f7a40bd34cffe3e0b58
Kami Wang <kmw143@duke.edu>
2023-12-08 16:23:33 +0800

    Updated code comments

--- nav.js
+++ nav.js
@@ -1,4 +1,4 @@
-// From https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
+// Adapted from https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
 
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
--- resume.js
+++ resume.js
@@ -1,12 +1,11 @@
-// From https://www.w3schools.com/howto/howto_js_accordion.asp
+// Adapted from https://www.w3schools.com/howto/howto_js_accordion.asp
 
-var acc = document.getElementsByClassName("accordion");
+var accor = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+for (i = 0; i < accor.length; i++) {
+  accor[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class, to highlight the button that controls the panel */
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
